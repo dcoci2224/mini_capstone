da98f43e722d6ccd61dc9b817a4e45fac1c5d0dd
Dan Coci <dcoci2224@gmail.com>
2020-03-03 20:36:02 -0600

    added views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,12 @@
 class Api::ProductsController < ApplicationController
+  def all_products_action
+    @message = "Welcome, enjoy the Fannie Mae Chocolate"
+    @all_products = Product.all
+    render "all_products.json.jb"
+  end
+
+  def first_product_action
+    @product = Product.first
+    render "first_product.json.jb"
+  end
 end
