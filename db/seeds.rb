3264f969764a66332e5001ae6103c161ac2d5ab4
Dan Coci <dcoci2224@gmail.com>
2020-03-03 19:00:39 -0600

    added products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+my_product = Product.new({ name: "Mint Meltaway", price: 10, description: "Mint/milk chocolate per pound" })
+my_product.save
+
+my_product1 = Product.new({ name: "Pixie", price: 12, description: "Milk chocolate with caramel and pecans per pound" })
+my_product1.save
+
+my_product2 = Product.new({ name: "Milk Chocolate Bars", price: 15, description: "10 milk chocolate bars" })
+my_product2.save
