e4ec92b5419b41bd7889e1c41e687dd34a412f3c
Dan Coci <dcoci2224@gmail.com>
2020-03-05 20:30:02 -0600

    added delete table

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -11,10 +11,27 @@ class Api::ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new
-    product.name = params["name"]
-    product.price = params["price"]
-    product.description = prarms["description"]
+    @product = Product.new(
+      @product.name = params["name"],
+      @product.price = params["price"],
+      @product.description = params["description"]
+    )
+    @product.save
     render "create.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.name = params["name"]
+    @product.price = params["price"]
+    @product.description = params["description"]
+    @product.save
+    render "show.json.jb"
+  end
+
+  def delete
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "Product destroyed!" }
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-my_product = Product.new({ name: "Mint Meltaway", price: 10, description: "Mint/milk chocolate per pound" })
+my_product = Product.new({ name: "Mint Meltaway", price: 10, description: "Mint/milk chocolate per pound." })
 my_product.save
 
-my_product1 = Product.new({ name: "Pixie", price: 12, description: "Milk chocolate with caramel and pecans per pound" })
-my_product1.save
-
-my_product2 = Product.new({ name: "Milk Chocolate Bars", price: 15, description: "10 milk chocolate bars" })
+my_product2 = Product.new({ name: "Pixie", price: 12, description: "Milk chocolate with caramel and pecans per pound." })
 my_product2.save
+
+my_product3 = Product.new({ name: "Milk Chocolate Bars", price: 15, description: "10 milk chocolate bars." })
+my_product3.save
+
+my_product4 = Product.new({ name: "Trinidads", price: 17, description: "Smooth chocolate ganache cneter smothered in a toasted coconut coating by the pound." })
+my_product4.save
+
+my_product5 = Product.new({ name: "Buckeyes", price: 16, description: "Creamy peanut butter center in our signature milk chocolate by the pound." })
+my_product5.save
