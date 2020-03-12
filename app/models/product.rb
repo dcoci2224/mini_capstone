b633bfd490ed855039121e56f98f2c7b43cdfc11
Dan Coci <dcoci2224@gmail.com>
2020-03-11 21:18:41 -0500

    added belongs to in product.rb model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,11 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..500 }  #in: means within
 
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+
   def discount
     price < 14
   end
